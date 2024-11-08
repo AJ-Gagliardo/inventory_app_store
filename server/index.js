bcb7ef0755d5214e56da8112a641d0a52ef9e123
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-11-08 13:36:45 -0800

    testing reverting some changes for railway

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ const app = express();
 const path = require("path");
 require("dotenv").config();
 const cors = require("cors");
-const pool = require("./db");
+// const pool = require("./db");
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+// });
+
+app.get("/", (req, res) => {
+  console.log("home");
+  res.send("home");
 });
 
 app.listen(PORT, () => {
